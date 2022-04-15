c36c47a206d8713f8c36c858cff1f3ecc94483eb
Zainab <ajumobizainab@gmail.com>
2022-04-15 11:49:39 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab4Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool lessThan100(int num1, int num2)
+            {
+                int result = num1 + num2;
+                if (result < 100)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            bool result1 = lessThan100(12,134); //You can change these parameters to check numbers whose sum are less than 100
+            Console.WriteLine(result1);
+
+            bool result2 = lessThan100(12,14); //You can change these parameters to check numbers whose sum are less than 100
+            Console.WriteLine(result2);
+        }
+    }
+}
